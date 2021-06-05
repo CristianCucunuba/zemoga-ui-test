d2445b0ac5fef0157cf309e1df5196f438aa46f9
cristiancc <cristiancamilo.cucunuba@gmail.com>
2021-06-05 11:57:16 -0500

    add tailwindcss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from "next/app";
+import "../styles/tailwind.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./src/pages/**/*tsx", "./src/components/**/*.tsx"],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
