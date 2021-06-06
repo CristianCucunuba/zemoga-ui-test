8b77a492b417865332fe050bedd07f0d3c0c8f1c
cristiancc <cristiancamilo.cucunuba@gmail.com>
2021-06-06 14:03:53 -0500

    add new font Lato

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   mode: "jit",
   purge: ["./src/pages/**/*.tsx", "./src/components/**/*.tsx"],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Lato", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},
